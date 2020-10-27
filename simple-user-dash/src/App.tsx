9057928fa1dd7e78717377f52e863c3df014bce9
yutveg <kodi.0n31l@gmail.com>
2020-10-27 14:59:35 -0700

    most of formik logic and components built out

--- simple-user-dash/src/App.tsx
+++ simple-user-dash/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      
+      <Register />
     </div>
   );
-}
+};
 
 export default App;
--- simple-user-dash/src/components/auth/Register.tsx
+++ simple-user-dash/src/components/auth/Register.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
-import { Formik } from 'formik';
-
+import { Formik, Field, Form } from 'formik';
+import { TextField, Button } from '@material-ui/core';
 const Register: React.FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Formik
+        initialValues={{ firstName: '', lastName: '' }}
+        onSubmit={(data, { setSubmitting, resetForm }) => {
+          setSubmitting(true);
+          // some async process
+          console.log(data);
+          resetForm();
+          setSubmitting(false);
+        }}
+      >
+        {({ values, isSubmitting }) => (
+          <Form>
+            <Field
+              placeholder="first name"
+              name="firstName"
+              type="input"
+              as={TextField}
+            />
+            <div>
+              <Field
+                placeholder="last name"
+                name="lastName"
+                type="input"
+                as={TextField}
+              />
+            </div>
+
+            <div>
+              <Button disabled={isSubmitting} type="submit">
+                Submit
+              </Button>
+            </div>
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
 export default Register;
