b7f3ce11aa2b2fa943136ff0565a9a5421be165d
yutveg <kodi.0n31l@gmail.com>
2020-10-28 16:45:30 -0700

    changed body background color and fixed margins for field inputs

--- simple-user-dash/src/components/auth/formStyles.tsx
+++ simple-user-dash/src/components/auth/formStyles.tsx
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const AuthForm = styled.div`
   margin: 300px auto;
   padding: 20px;
-  backgroundcolor: #fafafa;
+  background-color: #e0e0e0;
   width: 300px;
 
-  input {
-    margin: 20px 0;
+  * {
+    margin: 5px 0;
   }
 `;
