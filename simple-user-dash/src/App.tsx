a71cb0e56affaf407684be009cd23e9fc077f591
yutveg <kodi.0n31l@gmail.com>
2020-11-18 12:37:04 -0800

    added apollo hook from graphql code gen CLI

--- simple-user-dash/src/App.tsx
+++ simple-user-dash/src/App.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Dashboard from './components/dash/Dashboard';
+import { gql, useQuery } from '@apollo/react-hooks';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useHelloQuery } from './generated/graphql';
 
 const App: React.FC = () => {
+  const { data, loading } = useHelloQuery();
+
+  if (loading || !data) {
+    return <div>loading..</div>;
+  }
   return (
     <div className="App">
-      <BrowserRouter>
+      {data.hello}
+      {/* <BrowserRouter>
         <Switch>
           <Route path={`/dashboard`} component={Dashboard} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
         </Switch>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 };
